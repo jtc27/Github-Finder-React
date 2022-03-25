499066f69ce4b02838e2559ba3f8a9e64e0dbbe4
Jacob Cho <jacobcho@live.com>
2022-03-25 11:59:33 +0000

    footer made

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
     <div className='flex flex-col justify-between h-screen'>
       <Navbar />
 
-      <main>Content</main>
+      <main className='container mx-auto px-3 pb-12'>Content</main>
       
+      <Footer />
     </div>
   </Router>
   );
