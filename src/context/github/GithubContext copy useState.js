46921f3ec58ecebb5d981f9a7d61bdbe019bc7b3
Jacob Cho <jacobcho@live.com>
2022-03-25 22:39:31 +0000

    reducers made

--- src/context/github/GithubContext copy useState.js
+++ src/context/github/GithubContext copy useState.js
@@ -1,33 +1,26 @@
-import { createContext, useReducer } from "react";
-import githubReducer from "./GithubReducer";
+import { createContext, useState } from "react";
 
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 
 export const GithubProvider = ({children}) => {
-  const initialState = {
-    users: [],
-    loading: true
-  }
-
-  const [state, dispatch] = useReducer(githubReducer, initialState)
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const fetchUsers = async() => {
     const response = await fetch(`${GITHUB_URL}/users`)
 
     const data = await response.json()
 
-    dispatch({
-      type: 'GET_USERS',
-      payload: data
-    })
+    setUsers(data)
+    setLoading(false)
   }
 
 
   return <GithubContext.Provider value={{
-    users: state.users,
-    loading: state.loading,
+    users,
+    loading,
     fetchUsers
   }}>
     {children}
