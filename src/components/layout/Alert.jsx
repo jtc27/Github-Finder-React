faf87f9a025cb98a024ca9d9cf8b7dc0152a411f
Jacob Cho <jacobcho@live.com>
2022-03-28 23:12:21 +0100

    refactor

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -10,7 +10,7 @@ import AlertContext from "../../context/alert/AlertContext"
 
 function Alert() {
   const { alert } = useContext(AlertContext)
-
+ 
   return (
     <div
       className={`grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 mb-4`}
