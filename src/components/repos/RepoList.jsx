d6697fc1bc227d4bdd17a7b3664241cb9725dafa
Jacob Cho <jacobcho@live.com>
2022-03-29 03:11:39 +0100

    repoItem component made

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,4 +1,6 @@
- 
+import RepoItem from "./RepoItem"
+
+
 function RepoList({repos}) {
   return (
     <div className='rounded-lg shadow-lg card bg-base-100 text-black'>
@@ -7,7 +9,7 @@ function RepoList({repos}) {
           Latest Repositories
         </h2>
         {repos.map((repo) => (
-          <h3>{repo.name}</h3>
+          <RepoItem key={repo.id} repo={repo}/>
         ))}
       </div>
     </div>
